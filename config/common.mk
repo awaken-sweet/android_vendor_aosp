2b3c5c8ab13f47335e75e40335591f703477cee9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-29 13:15:23 +0530

    config: Build LMO Freeform app and sidebar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: GXC 2356 <gxcdev2356@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -174,6 +174,11 @@ PRODUCT_PACKAGES += \
     getcap \
     setcap
 
+# LMOFreeForm
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
+
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
     system/bin/getcap \
